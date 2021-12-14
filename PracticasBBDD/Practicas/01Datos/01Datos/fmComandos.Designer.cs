7443e7e1d63383abf704678f8f12f061bd1bdd4d
JoseASL <joseviki10@gmail.com>
2021-12-14 19:10:39 +0100

    Practica

--- PracticasBBDD/Practicas/01Datos/01Datos/fmComandos.Designer.cs
+++ PracticasBBDD/Practicas/01Datos/01Datos/fmComandos.Designer.cs
@@ -29,11 +29,238 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.bd_telefonosDataSet = new _01Datos.bd_telefonosDataSet();
+            this.telefonosBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.telefonosTableAdapter = new _01Datos.bd_telefonosDataSetTableAdapters.telefonosTableAdapter();
+            this.tableAdapterManager = new _01Datos.bd_telefonosDataSetTableAdapters.TableAdapterManager();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.dataGridViewTextBoxColumn8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dgvTelefonos = new System.Windows.Forms.DataGridView();
+            this.btPrimero = new System.Windows.Forms.Button();
+            this.btAnterior = new System.Windows.Forms.Button();
+            this.btSiguiente = new System.Windows.Forms.Button();
+            this.btUltimo = new System.Windows.Forms.Button();
+            this.stEstado = new System.Windows.Forms.StatusStrip();
+            this.laPosicion = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.bd_telefonosDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.telefonosBindingSource)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTelefonos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // bd_telefonosDataSet
+            // 
+            this.bd_telefonosDataSet.DataSetName = "bd_telefonosDataSet";
+            this.bd_telefonosDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // telefonosBindingSource
+            // 
+            this.telefonosBindingSource.DataMember = "telefonos";
+            this.telefonosBindingSource.DataSource = this.bd_telefonosDataSet;
+            // 
+            // telefonosTableAdapter
+            // 
+            this.telefonosTableAdapter.ClearBeforeFill = true;
+            // 
+            // tableAdapterManager
+            // 
+            this.tableAdapterManager.BackupDataSetBeforeUpdate = false;
+            this.tableAdapterManager.telefonosTableAdapter = this.telefonosTableAdapter;
+            this.tableAdapterManager.UpdateOrder = _01Datos.bd_telefonosDataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.laPosicion);
+            this.panel1.Controls.Add(this.btUltimo);
+            this.panel1.Controls.Add(this.btSiguiente);
+            this.panel1.Controls.Add(this.btAnterior);
+            this.panel1.Controls.Add(this.btPrimero);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(858, 73);
+            this.panel1.TabIndex = 0;
+            // 
+            // dataGridViewTextBoxColumn8
+            // 
+            this.dataGridViewTextBoxColumn8.DataPropertyName = "pais";
+            this.dataGridViewTextBoxColumn8.HeaderText = "pais";
+            this.dataGridViewTextBoxColumn8.Name = "dataGridViewTextBoxColumn8";
+            // 
+            // dataGridViewTextBoxColumn7
+            // 
+            this.dataGridViewTextBoxColumn7.DataPropertyName = "provincia";
+            this.dataGridViewTextBoxColumn7.HeaderText = "provincia";
+            this.dataGridViewTextBoxColumn7.Name = "dataGridViewTextBoxColumn7";
+            // 
+            // dataGridViewTextBoxColumn6
+            // 
+            this.dataGridViewTextBoxColumn6.DataPropertyName = "ciudad";
+            this.dataGridViewTextBoxColumn6.HeaderText = "ciudad";
+            this.dataGridViewTextBoxColumn6.Name = "dataGridViewTextBoxColumn6";
+            // 
+            // dataGridViewTextBoxColumn5
+            // 
+            this.dataGridViewTextBoxColumn5.DataPropertyName = "observaciones";
+            this.dataGridViewTextBoxColumn5.HeaderText = "observaciones";
+            this.dataGridViewTextBoxColumn5.Name = "dataGridViewTextBoxColumn5";
+            // 
+            // dataGridViewTextBoxColumn4
+            // 
+            this.dataGridViewTextBoxColumn4.DataPropertyName = "telefono";
+            this.dataGridViewTextBoxColumn4.HeaderText = "telefono";
+            this.dataGridViewTextBoxColumn4.Name = "dataGridViewTextBoxColumn4";
+            // 
+            // dataGridViewTextBoxColumn3
+            // 
+            this.dataGridViewTextBoxColumn3.DataPropertyName = "direccion";
+            this.dataGridViewTextBoxColumn3.HeaderText = "direccion";
+            this.dataGridViewTextBoxColumn3.Name = "dataGridViewTextBoxColumn3";
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.DataPropertyName = "nombre";
+            this.dataGridViewTextBoxColumn2.HeaderText = "nombre";
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.DataPropertyName = "id";
+            this.dataGridViewTextBoxColumn1.HeaderText = "id";
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            // 
+            // dgvTelefonos
+            // 
+            this.dgvTelefonos.AutoGenerateColumns = false;
+            this.dgvTelefonos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvTelefonos.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn1,
+            this.dataGridViewTextBoxColumn2,
+            this.dataGridViewTextBoxColumn3,
+            this.dataGridViewTextBoxColumn4,
+            this.dataGridViewTextBoxColumn5,
+            this.dataGridViewTextBoxColumn6,
+            this.dataGridViewTextBoxColumn7,
+            this.dataGridViewTextBoxColumn8});
+            this.dgvTelefonos.DataSource = this.telefonosBindingSource;
+            this.dgvTelefonos.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvTelefonos.Location = new System.Drawing.Point(0, 73);
+            this.dgvTelefonos.Name = "dgvTelefonos";
+            this.dgvTelefonos.RowTemplate.Height = 24;
+            this.dgvTelefonos.Size = new System.Drawing.Size(858, 423);
+            this.dgvTelefonos.TabIndex = 1;
+            this.dgvTelefonos.CellEnter += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvTelefonos_CellEnter);
+            // 
+            // btPrimero
+            // 
+            this.btPrimero.AutoSize = true;
+            this.btPrimero.Location = new System.Drawing.Point(11, 22);
+            this.btPrimero.Name = "btPrimero";
+            this.btPrimero.Size = new System.Drawing.Size(75, 27);
+            this.btPrimero.TabIndex = 0;
+            this.btPrimero.Text = "Primero";
+            this.btPrimero.UseVisualStyleBackColor = true;
+            this.btPrimero.Click += new System.EventHandler(this.btPrimero_Click);
+            // 
+            // btAnterior
+            // 
+            this.btAnterior.AutoSize = true;
+            this.btAnterior.Location = new System.Drawing.Point(103, 22);
+            this.btAnterior.Name = "btAnterior";
+            this.btAnterior.Size = new System.Drawing.Size(75, 27);
+            this.btAnterior.TabIndex = 1;
+            this.btAnterior.Text = "Anterior";
+            this.btAnterior.UseVisualStyleBackColor = true;
+            this.btAnterior.Click += new System.EventHandler(this.btAnterior_Click);
+            // 
+            // btSiguiente
+            // 
+            this.btSiguiente.AutoSize = true;
+            this.btSiguiente.Location = new System.Drawing.Point(251, 22);
+            this.btSiguiente.Name = "btSiguiente";
+            this.btSiguiente.Size = new System.Drawing.Size(77, 27);
+            this.btSiguiente.TabIndex = 2;
+            this.btSiguiente.Text = "Siguiente";
+            this.btSiguiente.UseVisualStyleBackColor = true;
+            this.btSiguiente.Click += new System.EventHandler(this.btSiguiente_Click);
+            // 
+            // btUltimo
+            // 
+            this.btUltimo.AutoSize = true;
+            this.btUltimo.Location = new System.Drawing.Point(343, 22);
+            this.btUltimo.Name = "btUltimo";
+            this.btUltimo.Size = new System.Drawing.Size(75, 27);
+            this.btUltimo.TabIndex = 3;
+            this.btUltimo.Text = "Último";
+            this.btUltimo.UseVisualStyleBackColor = true;
+            this.btUltimo.Click += new System.EventHandler(this.btUltimo_Click);
+            // 
+            // stEstado
+            // 
+            this.stEstado.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.stEstado.Location = new System.Drawing.Point(0, 474);
+            this.stEstado.Name = "stEstado";
+            this.stEstado.Size = new System.Drawing.Size(858, 22);
+            this.stEstado.TabIndex = 2;
+            this.stEstado.Text = "statusStrip1";
+            // 
+            // laPosicion
+            // 
+            this.laPosicion.AutoSize = true;
+            this.laPosicion.Location = new System.Drawing.Point(186, 28);
+            this.laPosicion.Name = "laPosicion";
+            this.laPosicion.Size = new System.Drawing.Size(48, 17);
+            this.laPosicion.TabIndex = 4;
+            this.laPosicion.Text = "0 de 0";
+            // 
+            // fmComandos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(858, 496);
+            this.Controls.Add(this.stEstado);
+            this.Controls.Add(this.dgvTelefonos);
+            this.Controls.Add(this.panel1);
+            this.Name = "fmComandos";
             this.Text = "fmComandos";
+            this.Load += new System.EventHandler(this.fmComandos_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.bd_telefonosDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.telefonosBindingSource)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTelefonos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private bd_telefonosDataSet bd_telefonosDataSet;
+        private System.Windows.Forms.BindingSource telefonosBindingSource;
+        private bd_telefonosDataSetTableAdapters.telefonosTableAdapter telefonosTableAdapter;
+        private bd_telefonosDataSetTableAdapters.TableAdapterManager tableAdapterManager;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn8;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn7;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridView dgvTelefonos;
+        private System.Windows.Forms.Button btPrimero;
+        private System.Windows.Forms.Button btUltimo;
+        private System.Windows.Forms.Button btSiguiente;
+        private System.Windows.Forms.Button btAnterior;
+        private System.Windows.Forms.StatusStrip stEstado;
+        private System.Windows.Forms.Label laPosicion;
     }
 }
--- PracticasBBDD/Practicas/01Datos/01Datos/fmComandos.cs
+++ PracticasBBDD/Practicas/01Datos/01Datos/fmComandos.cs
@@ -16,5 +16,59 @@ namespace _01Datos
         {
             InitializeComponent();
         }
+
+        private void telefonosBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.telefonosBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.bd_telefonosDataSet);
+
+        }
+
+        private void fmComandos_Load(object sender, EventArgs e)
+        {
+            // TODO: esta línea de código carga datos en la tabla 'bd_telefonosDataSet.telefonos' Puede moverla o quitarla según sea necesario.
+            this.telefonosTableAdapter.Fill(this.bd_telefonosDataSet.telefonos);
+        }
+
+        private void btPrimero_Click(object sender, EventArgs e)
+        {
+            telefonosBindingSource.MoveFirst();
+        }
+
+        private void btAnterior_Click(object sender, EventArgs e)
+        {
+            telefonosBindingSource.MovePrevious();
+        }
+
+        private void btSiguiente_Click(object sender, EventArgs e)
+        {
+            telefonosBindingSource.MoveNext();
+        }
+
+        private void btUltimo_Click(object sender, EventArgs e)
+        {
+            telefonosBindingSource.MoveLast();
+        }
+
+        private void mostrarPosicion()
+        {
+            int iTotal = telefonosBindingSource.Count;
+            int iPos;
+
+            if (iTotal == 0) {
+                laPosicion.Text = "0 de 0";
+            }
+            else
+            {
+                iPos = telefonosBindingSource.Position + 1;
+                laPosicion.Text = iPos.ToString() + " de " + iTotal.ToString();
+            }
+        }
+
+        private void dgvTelefonos_CellEnter(object sender, DataGridViewCellEventArgs e)
+        {
+            mostrarPosicion();
+        }
     }
 }
