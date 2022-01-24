27a818534e032474fef2e8332bd99625b16b023b
JoseASL <joseviki10@gmail.com>
2022-01-24 19:21:15 +0100

    din

---
+++ WPF/EjerciciosWPF/Ejercicio3/Ejercicio3/Ejercicio3/MainWindow.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ejercicio3
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void RbCochazo_Checked(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                imCoche.Source = new BitmapImage(new Uri("coche3.jpg", UriKind.Relative));
+            }catch(Exception err)
+            {
+                Console.WriteLine(err.Message);
+            }
+        }
+
+        private void RbAsequible_Checked(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                imCoche.Source = new BitmapImage(new Uri("coche2.jpg", UriKind.Relative));
+            }catch(Exception err)
+            {
+                Console.WriteLine(err.Message);
+            }
+        }
+
+        private void RbAceptable_Checked(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                imCoche.Source = new BitmapImage(new Uri("coche1.png", UriKind.Relative));
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine(err.Message);
+            }
+        }
+
+        private void CbPeliculas_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string movie = "";
+            int index = cbPeliculas.SelectedIndex;
+            switch (index)
+            {
+                case 0:
+                    movie = "avenger";
+                    break;
+                case 1:
+                    movie = "harry";
+                    break;
+                case 2:
+                    movie = "spidy";
+                    break;
+            }
+
+            try
+            {
+                imgPelis.Source = new BitmapImage(new Uri(movie + ".jpg", UriKind.Relative));
+            }catch(Exception err)
+            {
+                Console.WriteLine(err.Message);
+            }
+        }
+    }
+}
