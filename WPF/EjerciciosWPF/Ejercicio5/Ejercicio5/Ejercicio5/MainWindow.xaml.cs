b00485e0c69c156e0a3d1d27bab48598a0bbd067
JoseASL <joseviki10@gmail.com>
2022-01-27 20:49:28 +0100

    din

--- WPF/EjerciciosWPF/Ejercicio5/Ejercicio5/Ejercicio5/MainWindow.xaml.cs
+++ WPF/EjerciciosWPF/Ejercicio5/Ejercicio5/Ejercicio5/MainWindow.xaml.cs
@@ -20,9 +20,57 @@ namespace Ejercicio5
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        private int nl = 0;
+        private int nb = 0;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void BtNuevoLabel_Click(object sender, RoutedEventArgs e)
+        {
+            btNuevoLabel.Style = (Style)this.Resources["btVerde"];
+
+            Label l = new Label();
+            nl++;
+            l.Name = "label";
+            l.Content = "Hola " + nl;
+
+            lista.Children.Add(l);
+        }
+
+        private void BtNuevoBoton_Click(object sender, RoutedEventArgs e)
+        {
+            btNuevoBoton.Style = (Style)Resources["btVerde"];
+
+            Button bt = new Button();
+
+            nb++;
+            bt.Name = "botón";
+            bt.Content = "Botón " + nb;
+            lista.Children.Add(bt);
+            bt.Click += new RoutedEventHandler(evento_boton);
+        }
+
+        private void evento_boton(Object sender, RoutedEventArgs e)
+        {
+            string senderName;
+            senderName = sender.ToString();
+
+            MessageBox.Show("Hola soy el botón " + senderName);
+        }
+
+        private void BtBorrarElemento_Click(object sender, RoutedEventArgs e)
+        {
+            int i;
+
+            if (lista.Children.Count > 0)
+            {
+                i = lista.Children.Count - 1;
+                lista.Children.RemoveAt(i);
+            }
         }
     }
 }
