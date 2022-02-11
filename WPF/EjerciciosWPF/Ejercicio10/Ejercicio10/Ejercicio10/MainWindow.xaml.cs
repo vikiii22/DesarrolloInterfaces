56058cd7a9d5e13e94177f30ea7cc7e018557b59
JoseASL <joseviki10@gmail.com>
2022-02-11 18:47:55 +0100

    din

--- WPF/EjerciciosWPF/Ejercicio10/Ejercicio10/Ejercicio10/MainWindow.xaml.cs
+++ WPF/EjerciciosWPF/Ejercicio10/Ejercicio10/Ejercicio10/MainWindow.xaml.cs
@@ -29,10 +29,59 @@ namespace Ejercicio10
 
         private void BtAceptar_Click(object sender, RoutedEventArgs e)
         {
-            Usuario nuevoUsuario = new Usuario(tbNombre.Text, tbDNI.Text, tbUsuario.Text, "");
-            listaUsuarios.Add(nuevoUsuario);
-            dgUsuarios.ItemsSource = null;
-            dgUsuarios.ItemsSource = listaUsuarios;
+            /**
+             * comprobar que todos los campos tengan datos
+             * la contraseña requisito mínimo de 3 carácteres
+             * la contraseña debe coincidir
+             * los mensajes de error en el label inferior
+             */
+
+            if (tbNombre.Text == "")
+            {
+                lbInfo.Content = "Debes añadir un nombre";
+                tbNombre.Focus();
+            }
+            else if (tbDNI.Text=="") {
+                lbInfo.Content = "Debes añadir el DNI";
+                tbDNI.Focus();
+            }
+            else if (tbUsuario.Text=="")
+            {
+                lbInfo.Content = "Debes añadir el Usuario";
+                tbUsuario.Focus();
+            }
+            else if (pass.Password=="" || pass.Password.Length < 3)
+            {
+                lbInfo.Content = "Debes añadir una contraseña mayor a 3 carácteres";
+                pass.Focus();
+            }
+            else if(pass.Password != pass2.Password)
+            {
+                lbInfo.Content = "Las contraseñas tienen que ser iguales";
+                pass2.Focus();
+            }
+            else
+            {
+                Usuario nuevoUsuario = new Usuario(tbNombre.Text, tbDNI.Text, tbUsuario.Text, "");
+                listaUsuarios.Add(nuevoUsuario);
+                dgUsuarios.ItemsSource = null;
+                dgUsuarios.ItemsSource = listaUsuarios;
+                BorrarDatos();
+            }
+        }
+
+        private void TbNombre_KeyDown(object sender, KeyEventArgs e)
+        {
+            lbInfo.Content = "";
+        }
+
+        public void BorrarDatos()
+        {
+            tbNombre.Text = "";
+            tbDNI.Text = "";
+            tbUsuario.Text = "";
+            pass.Password = "";
+            pass2.Password = "";
         }
     }
 }
